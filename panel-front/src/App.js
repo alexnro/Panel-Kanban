c1c801a14aefa66c470e85b1dfb2b67fd2ddef70
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-06-04 09:52:35 +0200

    Header funcional

--- panel-front/src/App.js
+++ panel-front/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import Header from './containers/Header/Header';
+import Login from './containers/Login/Login';
+import Panel from './containers/Panel/Panel';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
-}
+
+const App = () => (
+
+  <BrowserRouter>
+      <Header />
+      <Route exact path="/login" component={Login} />
+      <Route path="/panel" component={Panel} />
+  </BrowserRouter>
+
+)
 
 export default App;
