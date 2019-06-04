1c663df0178fedd4a34df869bc56a88f2a1a0a80
alexnro <alejandro.navarro@ETOOL.TECH>
2019-06-04 14:00:35 +0200

    Conseguido hacer POST request a back-end, pero fallo en CORS

--- panel-front/src/containers/Login/Login.js
+++ panel-front/src/containers/Login/Login.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import axios from '../../axios-users';
 
 import './Login.css';
 
 class Login extends Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    submitHandler = event => {
+        event.preventDefault();
+        const data = {
+            Username: this.state.username,
+            Email: this.state.email,
+        }
+        const url = 'http://localhost:5000/login';
+        axios
+            .post(url, data)
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.log(error);
+                console.log(data);
+            });
+    }
+
     render() {
+
         return (
             <div className="formulario">
-                <form method="POST">
-                    <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                        </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit" value="Send">
-                            Submit
-                        </Button>
-                    </Form>
+                <form onSubmit={this.submitHandler}>
+                    <div className="form-group">
+                        <label htmlFor="email">Correo electrónico</label>
+                        <input id="email" type="email" name="email" className="form-control" aria-describedby="emailHelp" placeholder="Correo electrónico" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Contraseña</label>
+                        <input id="password" type="password" name="password" className="form-control" placeholder="Contraseña" required />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Iniciar sesión</button>
                 </form>
             </div>
         );
