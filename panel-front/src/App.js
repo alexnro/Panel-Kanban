916b6c8f93e786d3080deeedba4fef59490601a9
alexnro <alejandro.navarro@ETOOL.TECH>
2019-06-05 09:16:07 +0200

    Eliminado import no funcional

--- panel-front/src/App.js
+++ panel-front/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import './App.css';
 import Header from './containers/Header/Header';
 import Login from './containers/Login/Login';
 import Panel from './containers/Panel/Panel';
-import Tareas from './components/Tareas/Tareas';
+// import Tareas from './components/Tareas/Tareas';
 
 
 const App = () => (
@@ -13,7 +12,7 @@ const App = () => (
     <Header />
     <Route exact path="/login" component={Login} />
     <Route path="/panel" component={Panel} />
-    <Route path="/spring" component={Tareas} />
+    {/* <Route path="/spring" component={Tareas} /> */}
   </div>
 
 )
