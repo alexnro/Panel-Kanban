0fe29c72b3f2ee05b30601296572ed9e963dc13e
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-06-14 08:38:19 +0200

    Conseguido que cada tarea se coloque en su columna correspondiente

--- panel-front/src/containers/Tareas/Tareas.js
+++ panel-front/src/containers/Tareas/Tareas.js
@@ -1,19 +1,20 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Tareas extends Component {
+
     render() {
-        return(
+        return (
             <td>
                 <h2>{this.props.post.title}</h2>
                 <p>{this.props.post.message}</p>
-                <button 
-                    onClick={()=>this.props.dispatch({type:'EDIT_POST', id: this.props.post.id})}>
-                        Editar</button>
-                <button 
-                    onClick={() => this.props.dispatch({type:'DELETE_POST', id: this.props.post.id})}>
-                        Eliminar</button>
+                <p>Columna: {this.props.post.column}</p>
+                <button
+                    onClick={() => this.props.dispatch({ type: 'EDIT_POST', id: this.props.post.id })}>
+                    Editar</button>
+                <button
+                    onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: this.props.post.id })}>
+                    Eliminar</button>
             </td>
         );
     }
