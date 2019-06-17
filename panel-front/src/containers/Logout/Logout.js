f1e153be31e4f08e09946962ede5066e78e8cf8f
alexnro <alexjandron99@gmail.com>
2019-06-17 13:05:51 +0200

    Añadida lógica a logout para eliminar token de memoria

--- panel-front/src/containers/Logout/Logout.js
+++ panel-front/src/containers/Logout/Logout.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 class Logout extends Component {
+    
+    deleteTokenHandler = () => {
+        axios.post('/logout', localStorage.getItem("token"))
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    
     render() {
+        this.deleteTokenHandler();
+        localStorage.removeItem("token");
         return <Redirect to="/" />
     }
 }
