e3e16528a3658bd16ddc2ac9bfb403f66059e254
alexnro <alexjandron99@gmail.com>
2019-06-25 11:22:29 +0200

    Reorganizadas rutas

--- panel-back/app/auth/routes.py
+++ panel-back/app/auth/routes.py
@@ -1,6 +1,6 @@
 from flask import request
 from app.auth import bp
-from app.models import User, Task
+from app.models import User
 from werkzeug.security import generate_password_hash
 from flask_login import login_user, logout_user
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
@@ -50,41 +50,3 @@ def check_token():
     email = request.args.get("email")
     user = User.objects.get(email=email)
     return str(user.access_token)
-
-
-@bp.route('/getNewTaskId', methods=['GET'])
-def get_new_task_id():
-    tasks = Task.objects
-    aux = 0
-    for task in tasks:
-        aux += 1
-    return str(aux)
-
-
-@bp.route('/addTask', methods=['POST', 'GET'])
-def add_task():
-    task_id = request.args.get("task_id")
-    title = request.args.get("title")
-    message = request.args.get("message")
-    column = request.args.get("column")
-    task = Task(_id=task_id, title=title, message=message, column=column)
-    task.save()
-    return str(task)
-
-
-@bp.route('/deleteTask', methods=['POST', 'GET'])
-def delete_task():
-    task_id = request.args.get('task_id')
-    Task.objects.get(_id=task_id).delete()
-    return 'Task deleted'
-
-
-@bp.route('/updateTask', methods=['POST', 'GET'])
-def update_task():
-    task_id = request.args.get("task_id")
-    title = request.args.get("title")
-    message = request.args.get("message")
-    column = request.args.get("column")
-    task = Task.objects.get(_id=task_id)
-    task.update(title=title, message=message, column=column)
-    return 'Task updated'
