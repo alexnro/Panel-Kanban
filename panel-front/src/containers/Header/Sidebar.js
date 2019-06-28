a53b0884a58622c1b77c4f379f56b3e9db9bcf65
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-06-28 14:05:48 +0200

    separador añadido

--- panel-front/src/containers/Header/Sidebar.js
+++ panel-front/src/containers/Header/Sidebar.js
@@ -6,6 +6,11 @@ import './Sidebar.css';
 export default Sidebar => {
     return (
         <Menu>
+            <div>
+                <img src="logo" alt="holi" />
+                <p>Nombre</p>
+            </div>
+            <hr />
             <a href="/panel">Panel</a>
             <a href="/perfil">Perfil</a>
         </Menu>
