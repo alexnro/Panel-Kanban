860cce0cb648678f107e851288325af14a1fda04
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-07-04 08:55:11 +0200

    Modal Usuario implementado para que se cierre al enviar el formulario

--- panel-front/src/components/UI/Modal/ModalUsuario.js
+++ panel-front/src/components/UI/Modal/ModalUsuario.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Form } from 'react-bootstrap';
 
-import UserForm from '../../../containers/Usuario/UserForm';
+import axios from 'axios';
 
 class ModalUsuario extends Component {
     constructor(props, context) {
@@ -11,7 +11,11 @@ class ModalUsuario extends Component {
         this.handleClose = this.handleClose.bind(this);
 
         this.state = {
-            show: false
+            show: false,
+            value: '',
+            username: '',
+            cargo: '',
+            email: ''
         };
     }
 
@@ -21,6 +25,38 @@ class ModalUsuario extends Component {
 
     handleShow() {
         this.setState({ show: true });
+    }
+
+    componentWillMount() {
+        let queryParams = '?email=' + localStorage.getItem('email');
+        axios.get('/user' + queryParams)
+            .then(response => {
+                let username = response.data.username
+                let email = response.data.email
+                let cargo = response.data.cargo
+                this.setState({ ...this.state, username: username, email: email, cargo: cargo })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const username = this.getUsername.value;
+        const email = this.state.email;
+        const cargo = this.getCargo.value;
+        this.setState({ ...this.state, getUsername: username, email: email, cargo: cargo })
+        const queryParams = '?username=' + username + '&email=' + email + '&cargo=' + cargo;
+        console.log(queryParams);
+        axios.post('/updateUser' + queryParams)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        
     }
 
     render() {
@@ -34,7 +70,18 @@ class ModalUsuario extends Component {
                         <Modal.Title>Modificación Perfil</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <UserForm />
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group>
+                                <Form.Label>Nombre de usuario</Form.Label>
+                                <Form.Control type="text" ref={(input) => this.getUsername = input} placeholder="Usuario" defaultValue={this.state.username} />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Cargo</Form.Label>
+                                <Form.Control type="text" ref={(input) => this.getCargo = input} placeholder="Cargo" defaultValue={this.state.cargo} />
+                            </Form.Group>
+                            <Form.Label>{this.state.email}</Form.Label><br />
+                            <Button variant="outline-success" type="submit" onClick={this.handleClose}>Añadir</Button>
+                        </Form>
                     </Modal.Body>
                 </Modal>
             </>
