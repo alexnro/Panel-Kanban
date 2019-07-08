d5a61da8d96c0b6e397041de061e703674bf3811
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-07-08 13:18:35 +0200

    Modificación Imagen de perfil

--- panel-front/src/containers/Usuario/ImgPerfil.js
+++ panel-front/src/containers/Usuario/ImgPerfil.js
@@ -1,35 +1,14 @@
 import React, { Component } from 'react';
 import Avatar from 'react-avatar';
 
-import axios from 'axios';
-
 import './ImgPerfil.css';
 
 class ImgPerfil extends Component {
 
-    state = {
-        username: 'Sandra'
-    }
-
-    getUsername() {
-        let queryParams = '?email=' + localStorage.getItem('email');
-        console.log(queryParams)
-        axios.get('/user' + queryParams)
-            .then(response => {
-                console.log(response);
-                let username = response.data.username
-                this.setState({ ...this.state, username: username })
-                console.log(this.state)
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }
-
     render() {
         return (
             <span className="imagenPerfil">
-                <Avatar colors={['#ccccff', '#00ffff', '#ccffcc']}/>
+                <Avatar skypeId="sitebase" />
             </span>
         );
     }
