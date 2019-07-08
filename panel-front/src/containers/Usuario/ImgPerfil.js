de33f5f3d8353092241c0f86690a020ea2986504
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-07-08 13:55:53 +0200

    Imagen de perfil con inicial del nombre, por consola la letra

--- panel-front/src/containers/Usuario/ImgPerfil.js
+++ panel-front/src/containers/Usuario/ImgPerfil.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
-import Avatar from 'react-avatar';
+
+import classNames from 'classnames';
+import axios from 'axios';
 
 import './ImgPerfil.css';
 
 class ImgPerfil extends Component {
 
-    render() {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            username: ''
+        };
+    }
+
+    componentWillMount() {
+        let queryParams = '?email=' + localStorage.getItem('email');
+        axios.get('/user' + queryParams)
+            .then(response => {
+                let username = response.data.username
+                this.setState({ ...this.state, username: username })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    _getName(){
+        let name = this.state.username;
+        console.log(name);
+        return name
+                .split(' ')
+                .map( item => item.charAt(0) )
+                .slice(0,2)
+                .join(' ');
+    }
+
+    render(){
+        let { size }  = this.props;
+        let container = classNames('avatar',size);
         return (
-            <span className="imagenPerfil">
-                <Avatar skypeId="sitebase" />
-            </span>
+            <div className={container}>
+                <span>{this._getName()}</span>
+                {console.log(this._getName())}
+            </div>
         );
     }
 }
