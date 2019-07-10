76cfed733e5468ba5602005011eb5161236a8060
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-07-10 12:49:46 +0200

    Combine reducers añadido, funcional

--- panel-front/src/index.js
+++ panel-front/src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 
 import './index.css';
@@ -12,7 +12,12 @@ import App from './App';
 import Tareas from './store/reducers/Tareas';
 import Kanban from './store/reducers/Kanban';
 
-const store = createStore(Kanban);
+const rootReducer = combineReducers({
+    Tareas: Tareas,
+    Kanban: Kanban
+});
+
+const store = createStore(rootReducer);
 
 const app = (
     <Provider store={store}>
