893bf340ab9d73a8e0aa9112a9746517c8691d5e
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-07-10 12:56:23 +0200

    Imagen de perfil añadida y mejora css nombre usuario

--- panel-front/src/containers/Usuario/ImgPerfil.js
+++ panel-front/src/containers/Usuario/ImgPerfil.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import Avatar from 'react-avatar';
+import classNames from 'classnames';
+import axios from 'axios';
 
 import './ImgPerfil.css';
 
 class ImgPerfil extends Component {
 
-    render() {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            username: ''
+        };
+    }
+
+    componentWillMount() {
+        let queryParams = '?email=' + localStorage.getItem('email');
+        axios.get('/user' + queryParams)
+            .then(response => {
+                let username = response.data.username
+                this.setState({ ...this.state, username: username })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    _getName(){
+        let name = this.state.username;
+        return name
+                .split(' ')
+                .map( item => item.charAt(0) )
+                .slice(0,1)
+                .join(' ');
+    }
+
+    render(){
+        let { size }  = this.props;
+        let container = classNames('avatar',size);
         return (
-            <span className="imagenPerfil">
-                <Avatar skypeId="sitebase" />
+            <span className={container}>
+                <span>{this._getName()}</span>
             </span>
         );
     }
