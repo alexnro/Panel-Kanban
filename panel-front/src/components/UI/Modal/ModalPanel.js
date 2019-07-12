328d391494e947ee0888c15f82737ee1fee72835
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-07-12 13:48:54 +0200

    Tabulación modificada de 2 a 4

--- panel-front/src/components/UI/Modal/ModalPanel.js
+++ panel-front/src/components/UI/Modal/ModalPanel.js
@@ -3,63 +3,63 @@ import { Modal, Button, Form } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 class ModalPanel extends Component {
-  constructor(props, context) {
-    super(props, context);
+    constructor(props, context) {
+        super(props, context);
 
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
 
-    this.state = {
-      show: false,
-      value: '',
-      id: ''
-    };
-  }
-
-  handleClose() {
-    this.setState({ show: false });
-  }
-
-  handleShow() {
-    this.setState({ show: true });
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const name = this.getName.value;
-    const data = {
-        name
+        this.state = {
+            show: false,
+            value: '',
+            id: ''
+        };
     }
-    this.props.dispatch({
-      type: 'ADD_KANBAN', data
-    });
-    this.getName.value = '';
-  }
 
-  render() {
-    return (
-      <>
-        <Button variant="secondary" onClick={this.handleShow}>
-          Añadir Panel
+    handleClose() {
+        this.setState({ show: false });
+    }
+
+    handleShow() {
+        this.setState({ show: true });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const name = this.getName.value;
+        const data = {
+            name
+        }
+        this.props.dispatch({
+            type: 'ADD_KANBAN', data
+        });
+        this.getName.value = '';
+    }
+
+    render() {
+        return (
+            <>
+                <Button variant="secondary" onClick={this.handleShow}>
+                    Añadir Panel
           </Button>
 
-        <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Añadir Panel</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Group>
-                <Form.Label>Inserte un nombre para el panel</Form.Label>
-                <Form.Control required type="text" ref={(input) => this.getName = input} placeholder="Titulo panel" />
-              </Form.Group>
-              <Button variant="outline-secondary" type="submit" onClick={this.handleClose}>Añadir</Button>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      </>
-    );
-  }
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Añadir Panel</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group>
+                                <Form.Label>Inserte un nombre para el panel</Form.Label>
+                                <Form.Control required type="text" ref={(input) => this.getName = input} placeholder="Titulo panel" />
+                            </Form.Group>
+                            <Button variant="outline-secondary" type="submit" onClick={this.handleClose}>Añadir</Button>
+                        </Form>
+                    </Modal.Body>
+                </Modal>
+            </>
+        );
+    }
 }
 
 export default connect()(ModalPanel);
--- panel-front/src/components/UI/Modal/ModalTarea.js
+++ panel-front/src/components/UI/Modal/ModalTarea.js
@@ -3,82 +3,82 @@ import { Modal, Button, Form } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 class ModalTarea extends Component {
-  constructor(props, context) {
-    super(props, context);
+    constructor(props, context) {
+        super(props, context);
 
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
 
-    this.state = {
-      show: false,
-      value: '1',
-      id: ''
-    };
-  }
-
-  handleClose() {
-    this.setState({ show: false });
-  }
-
-  handleShow() {
-    this.setState({ show: true });
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const title = this.getTitle.value;
-    const message = this.getMessage.value;
-    const data = {
-      id: String(new Date()),
-      title,
-      message,
-      column: this.state.value
+        this.state = {
+            show: false,
+            value: '1',
+            id: ''
+        };
     }
-    this.props.dispatch({
-      type: 'ADD_POST', data
-    });
-    this.getTitle.value = '';
-    this.getMessage.value = '';
-  }
 
-  render() {
-    return (
-      <>
-        <Button variant="secondary" onClick={this.handleShow}>
-          Añadir Tarea
+    handleClose() {
+        this.setState({ show: false });
+    }
+
+    handleShow() {
+        this.setState({ show: true });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const title = this.getTitle.value;
+        const message = this.getMessage.value;
+        const data = {
+            id: String(new Date()),
+            title,
+            message,
+            column: this.state.value
+        }
+        this.props.dispatch({
+            type: 'ADD_POST', data
+        });
+        this.getTitle.value = '';
+        this.getMessage.value = '';
+    }
+
+    render() {
+        return (
+            <>
+                <Button variant="secondary" onClick={this.handleShow}>
+                    Añadir Tarea
           </Button>
 
-        <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Añadir Tarea</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Group>
-                <Form.Label>Inserte un título para la tarea</Form.Label>
-                <Form.Control required type="text" ref={(input) => this.getTitle = input} placeholder="Titulo tarea" />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Inserte una descripción para la tarea</Form.Label>
-                <Form.Control required as="textarea" ref={(input) => this.getMessage = input} placeholder="Descripción tarea" />
-              </Form.Group>
-              <Form.Group onChange={(e) => this.setState({ ...this.state, value: e.target.value })}>
-                <Form.Label>Seleccione una columna donde añadir la tarea</Form.Label><br />
-                <Form.Control as="select">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                </Form.Control>
-              </Form.Group>
-              <Button variant="outline-secondary" type="submit" onClick={this.handleClose}>Añadir</Button>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      </>
-    );
-  }
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Añadir Tarea</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group>
+                                <Form.Label>Inserte un título para la tarea</Form.Label>
+                                <Form.Control required type="text" ref={(input) => this.getTitle = input} placeholder="Titulo tarea" />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Inserte una descripción para la tarea</Form.Label>
+                                <Form.Control required as="textarea" ref={(input) => this.getMessage = input} placeholder="Descripción tarea" />
+                            </Form.Group>
+                            <Form.Group onChange={(e) => this.setState({ ...this.state, value: e.target.value })}>
+                                <Form.Label>Seleccione una columna donde añadir la tarea</Form.Label><br />
+                                <Form.Control as="select">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Button variant="outline-secondary" type="submit" onClick={this.handleClose}>Añadir</Button>
+                        </Form>
+                    </Modal.Body>
+                </Modal>
+            </>
+        );
+    }
 }
 
 export default connect()(ModalTarea);
