688cda6cf2bc9a09fc7fe82ccd256c95ede6c5e3
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-07-15 14:40:16 +0200

    Corrección errores ruta de los enlaces

--- panel-front/src/App.js
+++ panel-front/src/App.js
@@ -12,7 +12,7 @@ const App = () => (
         <Route exact path="/" component={Perfil} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        <Route exact path="/panel?name=" component={Panel} />
+        <Route exact path="/panel" component={Panel} />
         <Route exact path="/logout" component={Logout} />
         <Route exact path="/perfil" component={Perfil} />
     </div>
