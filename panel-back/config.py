6a98bdaa88a662624ee5c599d4210ae6596cc2d4
sandra.cabrera <sandracabreragarcia26@gmail.com>
2019-07-17 12:49:50 +0200

    Modificación bbdd

--- panel-back/config.py
+++ panel-back/config.py
@@ -8,6 +8,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     MONGODB_SETTINGS = {
         'db': 'kanban',
-        'host': 'mongodb+srv://admin:123@panel-kanban-5mm05.mongodb.net/kanban?retryWrites=true&w=majority'
+        'host': 'mongodb+srv://admin:123@cluster0-5mm05.mongodb.net/kanban?retryWrites=true&w=majority'
     }
-    MONGO_URI = 'mongodb+srv://admin:123@panel-kanban-5mm05.mongodb.net/kanban?retryWrites=true&w=majority'
+    MONGO_URI = 'mongodb+srv://admin:123@cluster0-5mm05.mongodb.net/kanban?retryWrites=true&w=majority'
